5b7313a237b3f45394a82a60e4df09fccef10a55
Luan Teixeira <luanlts@outlook.com>
2022-03-23 17:13:03 -0300

    criado o compontente algular sem usar o cli e ajustado o npm start para iniciar o projeto e ja abrir o navegador

---
+++ src/app/ola-angular/ola-angular.component.ts
@@ -0,0 +1,18 @@
+// definicao da classe em ts
+// export deixa a classe visivel para outras classes(semelhante ao public das classes Java)
+
+import { Component } from "@angular/core";
+
+//Anotações são metadados e servem para fazer configuração
+//() chama o construtor do componente e passa como parametro o objeto JSON de configurações
+@Component({
+    selector: 'ola-angular', // nome que o componente vai ter para poder ser usado no template
+    //template: '<p>Olá, {{framework}}</p>', // tudo que ira aparecer visualmente no componente
+    templateUrl: './ola-angular.component.html', // caminho para um template externo
+    styleUrls: [
+        './ola-angular.component.css'
+    ]
+})
+export class OlaAngularComponent{
+    framework = 'React'
+}
